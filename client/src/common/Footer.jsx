5c66ea1d5c02e20815e2af658302eee3bbba4f6e
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-04-29 18:57:29 +0300

    fix postion footer

--- client/src/common/Footer.jsx
+++ client/src/common/Footer.jsx
@@ -3,36 +3,37 @@ import {HashLink} from "react-router-hash-link";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between items-center px-4 sm:px-10 md:px-20 w-full py-2 mt-[700px] md:mt-0 bg-[#c9b289] text-black">
-      <HashLink to="/" className="w-48 bg-[#222222] px-2">
+    <footer className="flex justify-between items-center px-4 sm:px-10 md:px-20 w-full py-2 mt-[700px] md:mt-0 bg-black">
+      <HashLink to="/" className="w-48">
         <img alt="logo_ar" src="/logo_ar.webp" className="w-full" />
       </HashLink>
-      <ul className="flex-row-reverse justify-center items-center gap-4 bg-[#c9b289]  p-4 font-semibold  hidden lg:flex">
-        <div className=" flex-row-reverse justify-center items-center gap-4">
-          <HashLink className="text-sm lg:text-md" to={"/#"}>
-            الصفحة الرئيسية
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#aboutus"}>
-            من نحن
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#AddedValues"}>
-            قيمنا
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#projects"}>
-            مشاريعنا
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#ourTeam"}>
-            فريقنا
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#ServiceRequest"}>
-            طلب خدمة
-          </HashLink>
-          <HashLink className="text-sm lg:text-md" to={"/#contactUs"}>
-            تواصل معنا
-          </HashLink>
-        </div>
+      <ul
+        dir="rtl"
+        className="justify-center items-center gap-4 bg-[#c9b289] p-4 font-semibold  hidden lg:flex"
+      >
+        <HashLink className="text-sm lg:text-md" to={"/#"}>
+          الصفحة الرئيسية
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#aboutus"}>
+          من نحن
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#AddedValues"}>
+          قيمنا
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#projects"}>
+          مشاريعنا
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#ourTeam"}>
+          فريقنا
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#ServiceRequest"}>
+          طلب خدمة
+        </HashLink>
+        <HashLink className="text-sm lg:text-md" to={"/#contactUs"}>
+          تواصل معنا
+        </HashLink>
       </ul>
-      <ul className="flex justify-center items-center gap-2 text-black">
+      <ul className="flex justify-center items-center gap-2 text-white">
         <FaInstagram size={20} className="cursor-pointer" />
         <FaFacebook size={20} className="cursor-pointer" />
         <FaTwitter size={20} className="cursor-pointer" />
