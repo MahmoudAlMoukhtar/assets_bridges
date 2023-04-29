36999b842bad800c46e856221909c610a87fbb56
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-04-29 09:45:26 +0300

    update design service form

--- client/src/components/ServiceRequest.jsx
+++ client/src/components/ServiceRequest.jsx
@@ -19,7 +19,7 @@ const ServiceRequest = () => {
             placeholder="البريد الالكتروني"
           />
         </div>
-        <div className="flex flex-col gap-2 p-4 shadow-sm shadow-black">
+        <div id="first" className="flex flex-col gap-2 p-4 shadow-sm shadow-black">
           <h6 className="text-2xl font-semibold text-[#c9b289]">
             معلومات مقدم الطلب
           </h6>
@@ -34,26 +34,26 @@ const ServiceRequest = () => {
               />
             </div>
             <div className="flex flex-col w-1/2">
-              <label>الصفة</label>
-              <select className="w-full text-black bg-[#c9b289] cursor-pointer">
-                <option value="مالك الأرض">مالك الأرض</option>
-                <option value="وكيل">وكيل</option>
-                <option value="وسيط">وسيط</option>
-                <option value="آخر">آخر</option>
-              </select>
+              <label>رقم الجوال</label>
+              <input
+                required
+                type="number"
+                className="p-2 w-full text-black bg-[#c9b289]"
+                placeholder="رقم الجوال"
+              />
             </div>
           </div>
-          <div className="flex flex-col">
-            <label>رقم الجوال</label>
-            <input
-              required
-              type="number"
-              className="p-2 w-full text-black bg-[#c9b289]"
-              placeholder="رقم الجوال"
-            />
+          <div className="flex flex-col w-full">
+            <label>الصفة</label>
+            <select className="w-full text-black bg-[#c9b289] cursor-pointer">
+              <option value="مالك الأرض">مالك الأرض</option>
+              <option value="وكيل">وكيل</option>
+              <option value="وسيط">وسيط</option>
+              <option value="آخر">آخر</option>
+            </select>
           </div>
         </div>
-        <div className="flex flex-col gap-2 p-4 shadow-sm shadow-black">
+        <div id="second" className="flex flex-col gap-2 p-4 shadow-sm shadow-black">
           <h6 className="text-2xl font-semibold text-[#c9b289]">
             معلومات الطلب
           </h6>
@@ -67,25 +67,26 @@ const ServiceRequest = () => {
                 <option value="اخرى">اخرى</option>
               </select>
             </div>
+
             <div className="flex flex-col w-1/2">
-              <label>المساحة</label>
-              <input
-                required
-                type="number"
-                className="p-2 w-full text-black bg-[#c9b289]"
-                placeholder="المساحة"
-              />
+              <label>الهدف من العرض</label>
+              <select className="w-full text-black bg-[#c9b289] cursor-pointer">
+                <option value="المشاركة بكامل الأرض في الصندوق">
+                  المشاركة بكامل الأرض في الصندوق
+                </option>
+                <option value="البيع">البيع</option>
+                <option value="المشاركة الجزئية">المشاركة الجزئية</option>
+              </select>
             </div>
           </div>
-          <div className="flex flex-col">
-            <label>الهدف من العرض</label>
-            <select className="w-full text-black bg-[#c9b289] cursor-pointer">
-              <option value="المشاركة بكامل الأرض في الصندوق">
-                المشاركة بكامل الأرض في الصندوق
-              </option>
-              <option value="البيع">البيع</option>
-              <option value="المشاركة الجزئية">المشاركة الجزئية</option>
-            </select>
+          <div className="flex flex-col w-full">
+            <label>المساحة</label>
+            <input
+              required
+              type="number"
+              className="p-2 w-full text-black bg-[#c9b289]"
+              placeholder="المساحة"
+            />
           </div>
         </div>
         <button className="bg-[#c9b289] p-2 text-black font-semibold shadow-sm shadow-black mt-2 w-full">
