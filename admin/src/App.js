cddf9f0980767bd18688895e62656fb6a761e6b4
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-05-08 11:39:22 +0300

    test2

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,18 @@
-import Sidebar from "./components/sidebar/Sidebar";
+//import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
-import Home from "./pages/home/Home";
+//import Home from "./pages/home/Home";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import ContactMessagesList from "./pages/contactMessages/ContactMessagesList";
-import Message from "./pages/message/Message";
-import {ToastContainer} from "react-toastify";
-import NavbarModal from "./components/NavModal";
 import {useState} from "react";
-import ServicesRequests from "./pages/servicesRequests/ServicesRequest";
-import Request from "./pages/servicesRequests/Request";
-import Auth from "./pages/auth/Auth";
-import PrivaitRoute from "./components/PrivaitRoute";
-import ChangePassword from "./pages/ChangePassword/ChangePassword";
+// import ContactMessagesList from "./pages/contactMessages/ContactMessagesList";
+// import Message from "./pages/message/Message";
+// import {ToastContainer} from "react-toastify";
+// import NavbarModal from "./components/NavModal";
+// import ServicesRequests from "./pages/servicesRequests/ServicesRequest";
+// import Request from "./pages/servicesRequests/Request";
+// import Auth from "./pages/auth/Auth";
+// import PrivaitRoute from "./components/PrivaitRoute";
+// import ChangePassword from "./pages/ChangePassword/ChangePassword";
 
 function App() {
   const [theme, selectTheme] = useState("black");
